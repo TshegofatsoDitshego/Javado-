6c290f45a28c00099bcfb7fd75f11744ef8929b5
tsditshjhb024 <tsditshjhb024@student.wethinkcode.co.za>
2025-02-17 14:12:18 +0200

    Teir1 completed

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,25 +1,29 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
 
     public TaskManager() {
-        // Initialize tasks list
+        tasks = new ArrayList<>();// Initialize tasks list
+
     }
 
     public void addTask(String task) {
-        throw new UnsupportedOperationException("Implement this method!");
+        tasks.add(task);
     }
 
     public List<String> listTasks() {
-        throw new UnsupportedOperationException("Implement this method!");
+        return tasks;
     }
 
     public void deleteTask(String task){
 //        leave for iteration 4
+        tasks.remove(task);
     }
 
     public void exit() {
         // leave for iteration 2
+        System.out.println("Saving tasks and exiting...");
     }
 }
